3957ddecc6a81caeca936e449e40639cba3a8943
harukam122 <mashar0122.yhm@gmail.com>
2022-12-11 14:37:32 -0500

    intro layout fixed

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,8 +3,8 @@ import { TypeAnimation } from 'react-type-animation';
 
 export default function Intro() {
   return (
-    <section>
-        <div className="bg-white mx-auto flex px-16 sm:px-32 md:px-36 lg:px-48 py-20 md:flex-row flex-col justify-between items-center min-h-[90vh]">
+    <section className="flex justify-center mx-auto">
+        <div className="bg-white flex sm:max-w-[80%] max-w-[80%] py-20 md:flex-row flex-col md:justify-between items-center min-h-[90vh]">
             <div className="lg:flex-grow md:w-1/2 lg:pr-36 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className="text-gray-700 mb-4 text-4xl md:text-6xl font-bold">
                     Hi, I'm Haruka! 👋🏻
@@ -32,7 +32,7 @@ export default function Intro() {
                     Scroll down &#11015;&#65039; to see some of my projects :)
                 </p>
             </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <div className="lg:max-w-[50%] md:max-w-[40%] max-w-[60%]">
                 <img
                     className="object-cover object-center rounded"
                     alt="memoji"
