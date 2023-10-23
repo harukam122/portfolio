1a799752a04d8563f93e9882c70559ed008461b6
harukam122 <mashar0122.yhm@gmail.com>
2023-10-22 23:39:07 -0400

    feat: ip, details not yet done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, About, Experience, NoPage, Redesign, JamzFindz, Personas, Hello, SpeechMe, Syntax, Shell, Ghiblify } from "./pages/pages";
+import { Home, About, Experience, NoPage, Redesign, JamzFindz, Personas, Hello, SpeechMe, Syntax, Shell, Ghiblify, IP } from "./pages/pages";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -26,6 +26,7 @@ function App() {
           <Route path="/syntax" element={<Syntax scrollToTop={scrollToTop} />} />
           <Route path="/shell" element={<Shell scrollToTop={scrollToTop} />} />
           <Route path="/ghiblify" element={<Ghiblify scrollToTop={scrollToTop} />} />
+          <Route path="/ip" element={<IP scrollToTop={scrollToTop} />} />
           <Route path="/about" element={<About scrollToTop={scrollToTop} />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="*" element={<NoPage />} />
